d0bee6ed34fef55e2193eb2e875c8834d45bc5c1
Anastasiya Olhovik <93829935+AnastasiyaOlhovik@users.noreply.github.com>
2022-11-24 22:55:12 +0300

    add

---
+++ src/components/App.jsx
@@ -0,0 +1,83 @@
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import { nanoid } from 'nanoid';
+import "./App.module.css";
+
+
+import { useState, useEffect } from "react";
+
+
+export default function App() {
+  const [contacts, setContacts] = useState(() => {
+    return localStorage.getItem("contacts")
+      ? JSON.parse(localStorage.getItem("contacts"))
+      : [
+        { id: "id-111", name: "Rosie Simpson", number: "459-12-56" },
+        { id: "id-211", name: "Hermione Kline", number: "443-89-12" },
+        { id: "id-311", name: "Eden Clements", number: "645-17-79" },
+        { id: "id-411", name: "Annie Copeland", number: "227-91-26" },
+      ];
+
+  });
+
+  const [filter, setFilter] = useState('');
+
+  const addContact = (name, number) => {
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    if (contacts.find((contact) => contact.name === newContact.name)) {
+      alert(`${newContact.name} is already in contacts.`);
+      return;
+    }
+
+    setContacts([newContact, ...contacts]);
+  };
+
+
+
+  const changeFilter = (event) => {
+    setFilter(event.currentTarget.value);
+  };
+
+
+
+  const deleteContact = (contactId) => {
+    setContacts(
+      contacts.filter((contact) => contact.id !== contactId));
+  };
+
+
+  const getVisibleContacts = () => {
+    return contacts.filter((contact) => contact.name.toLowerCase().includes(filter.toLowerCase()));
+  };
+
+  const visibleContacts = getVisibleContacts();
+
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  return (
+    < >
+      <h1>Phonebook</h1>
+
+      <ContactForm onSubmit={addContact} />
+
+      <h2>Contants</h2>
+      <Filter value={filter} onChange={changeFilter} />
+
+      <ContactList
+        contacts={visibleContacts}
+        onDeleteContact={deleteContact}
+      ></ContactList>
+
+
+    </ >
+  );
+}
+
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,73 @@
+import css from './ContactForm.module.css';
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+export default function ContactForm({ onSubmit }) {
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
+
+    const handleChange = (event) => {
+
+        const { name, value } = event.currentTarget;
+        switch (name) {
+            case 'name':
+                setName(value);
+                break;
+            case 'number':
+                setNumber(value);
+                break;
+
+            default:
+                break;
+        }
+    };
+
+    const handleSubmit = (event) => {
+
+        event.preventDefault();
+        onSubmit(name, number);
+        setName('');
+        setNumber('');
+    };
+
+
+    return (
+        <form className={css.form} onSubmit={handleSubmit}>
+            <label className={css.label} htmlFor="">
+                Name
+                <input
+                    // ref={this.textInput}
+                    className={css.input}
+                    type="text"
+                    name="name"
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    value={name}
+                    onChange={handleChange}
+                    title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+                    required
+                />
+            </label>
+            <label className={css.label} htmlFor="">
+                Number
+                <input
+                    className={css.input}
+                    type="tel"
+                    name="number"
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    value={number}
+                    onChange={handleChange}
+                    title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+                    required
+                />
+            </label>
+
+            <button className={css.button} type="submit">
+                <span>Add contact</span>
+            </button>
+        </form>
+    );
+}
+
+ContactForm.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+}
